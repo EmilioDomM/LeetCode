c13bfe6a51baaf3fd805c00ca90a57c3b7efa335
EmiDMoreno <A01571423@tec.mx>
2023-11-01 11:34:02 -0600

    add(Two_Sum)

---
+++ LEETCODE/Easy/1. Two Sum/Two_Sum.cpp
@@ -0,0 +1,25 @@
+/*
+    May / 09 / 2023
+    1. Two Sum
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+       
+        for(int i = 0; i < nums.size(); i++){
+
+            if(mp.find(target - nums[i]) == mp.end()){
+                mp[nums[i]] = i;
+            }else { 
+
+                return {mp[target - nums[i]], i}; 
+            }
+        }
+ 
+        return {};
+
+    }
+};
